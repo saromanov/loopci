f39c7b22eef588c33d271cd1c436b600dcd71207
Sergey Romanov <xxsmotur@gmail.com>
2015-10-26 07:39:50 -0700

    config: Update

--- loopci/config.py
+++ loopci/config.py
@@ -1,11 +1,11 @@
-import pyhcl
+import hcl
 import os
 
 def loadConfig(path):
     name = '.loopci.hcl'
     if len([f for f in os.listdir(path) if f == name]) != 1:
-        raise Exception('{0} is not found', name)
+        raise Exception('{0} is not found'.format(name))
     f = open(os.path.join(path, name), 'r')
-    obj = pyhcl.hcl.load(f.read())
+    obj = hcl.load(f.read())
     f.close()
     return obj
