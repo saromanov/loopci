6a544597d4737f40ca2dab859a3917bb0efdc5f5
Sergey Romanov <xxsmotur@gmail.com>
2015-10-26 07:34:09 -0700

    Add logging

--- loopci/loopci.py
+++ loopci/loopci.py
@@ -3,8 +3,11 @@ import construct
 import asyncio
 import os
 import math
+import logging
 
 import docker
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Loopci:
     def __init__(self, *args, **kwargs):
@@ -17,6 +20,7 @@ class Loopci:
         return os.path.isdir(path)
 
     def run(self, path):
+        logging.debug("Checking directory")
         if self._dirExist(path) is False:
             raise Exception("Directory {0} is not exist".format(path))
         current_dir = self._createDir()
