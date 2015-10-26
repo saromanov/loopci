46fdbc5a2d8cb184ced12511b76998c9e785f9b8
Sergey Romanov <xxsmotur@gmail.com>
2015-10-26 09:18:42 -0700

    Add restrict construction

--- loopci/construct.py
+++ loopci/construct.py
@@ -4,19 +4,31 @@
 UBUNTU_INSTALL = 'apt-get install'
 FEDORA_INSTALL = 'yum install'
 
+class BaseConstruct:
+    def __init__(self):
+        self.result = ''
 
-class Construct:
+    def createDockerfile(self, path):
+        f = open(path + '/Dockerfile', 'w')
+        f.write(self.result)
+        f.close()
+
+
+class Construct(BaseConstruct):
     ''' Construct provides construction of Docker file
+        It can construct from images or step by step
     '''
     def __init__(self, descr):
+        BaseConstruct.__init__(self)
         self.description= descr
         self.result = ""
 
         #Set default os
         self.ostitle = "Ubuntu"
         self.osver = "14.04"
+        self.commands = {}
 
-    def addOS(self, title, version):
+    def addOS(self, title, version='latest'):
         self.result += "FROM {0} {1}\n".format(title, version)
         self.ostitle = title
         self.osver = version
@@ -27,9 +39,15 @@ class Construct:
         if self.ostitle in ['fedora', 'centos', 'mageia']:
             return FEDORA_INSTALL
 
-    def addLanguage(self, title, version):
+    def addLanguage(self, title, version='0'):
         install = self._getInstallType()
         strdata = ''
+        #Check if the title contans version
+        #For example go:1.5 or python:3.5
+        if title.find(':') != -1:
+            items = title.split(':')
+            title = items[0]
+            version = items[1]
         if title == 'python':
             strdata += '{0} python'.format(install)
             strdata += '{0} python-dev'.format(install)
@@ -37,8 +55,10 @@ class Construct:
         if title == 'node':
             strdata += '{0} install npm'.format(install)
             strdata += '{0} install nodejs'.format(install)
-            strdata += 'nvm install {0}'.format(version)
-            strdata += 'nvm use {0}'.format(version)
+
+            if version is not '0':
+                strdata += 'nvm install {0}'.format(version)
+                strdata += 'nvm use {0}'.format(version)
 
         if title == 'go':
             strdata += '{0} install golang'.format(install)
@@ -54,3 +74,23 @@ class Construct:
         f = open(path + '/Dockerfile', 'w')
         f.write(self.result)
         f.close()
+
+
+class RestrictConstruct(BaseConstruct):
+    def __init__(self, descr):
+        BaseConstruct.__init__(self)
+        self.result = ''
+
+    def addLanguage(self, title):
+        if title.find('go') != -1:
+            lang = "golang"
+            version = title.split(':')[1]
+            self.result += 'FROM {0}:{1}'.format(lang, version)
+
+        if title.find('python') != -1:
+            self.result += 'FROM {0}'.format(title)
+
+        if title.find('node') != -1:
+            self.result += 'FROM {0}'.format(title)
+
+
