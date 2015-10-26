19dbd6c8fac7b6392e33f0a21014f08452d0b8df
Sergey Romanov <xxsmotur@gmail.com>
2015-10-26 07:29:17 -0700

    Checking exist of directory

--- loopci/loopci.py
+++ loopci/loopci.py
@@ -7,17 +7,23 @@ import math
 import docker
 
 class Loopci:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         pass
 
     def _createDir(self):
         return "dir"
 
-    def run(self):
+    def _dirExist(self, path):
+        return os.path.isdir(path)
+
+    def run(self, path):
+        if self._dirExist(path) is False:
+            raise Exception("Directory {0} is not exist".format(path))
         current_dir = self._createDir()
         os.chdir(current_dir)
         cons = Construct("Fun construction")
         dock = Docker()
         dock.start('.')
-        docker = sh.Command("sudo docker -t build")
-        docker()
+        manager = docker.DockerManager()
+        #docker = sh.Command("sudo docker -t build")
+        #docker()
