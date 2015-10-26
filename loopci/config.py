21a6eac2154863ec430271ba2caaa745e1883823
Sergey Romanov <xxsmotur@gmail.com>
2015-10-26 07:43:19 -0700

    Update

--- loopci/config.py
+++ loopci/config.py
@@ -6,6 +6,6 @@ def loadConfig(path):
     if len([f for f in os.listdir(path) if f == name]) != 1:
         raise Exception('{0} is not found'.format(name))
     f = open(os.path.join(path, name), 'r')
-    obj = hcl.load(f.read())
+    obj = hcl.load(f)
     f.close()
     return obj
