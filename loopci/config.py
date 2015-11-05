7e0abcf77db12eaef565090d20e5f4a21dbce01f
Sergey Romanov <xxsmotur@gmail.com>
2015-11-05 08:13:24 -0800

    Set load config in json format as optional

--- loopci/config.py
+++ loopci/config.py
@@ -9,3 +9,14 @@ def loadConfig(path):
     obj = hcl.load(f)
     f.close()
     return obj
+
+def loadJSONConfig(path):
+    ''' optional. load config in json format
+    '''
+    name = '.loopci.json'
+    if len([f for f in os.listdir(path) if f == name]) != 1:
+        raise Exception('{0} is not found'.format(name))
+    f = open(os.path.join(path, name), 'r')
+    obj = json.loads(f.read())
+    f.close()
+    return obj
