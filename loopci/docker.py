9ce490293388a945c3606ea92b94750613eab6bb
Sergey Romanov <xxsmotur@gmail.com>
2015-11-16 06:56:54 -0800

    DockerManager: Update

--- loopci/docker.py
+++ loopci/docker.py
@@ -6,6 +6,10 @@ class DockerManager:
     ''' This class provides all about management with docker
         This class starts after construction of Dockerfile
     '''
+    def __init__(self, *args, **kwargs):
+        ''' TODO: Provide options for create of container
+        '''
+        pass
 
     def start(path):
         ''' build end run docker image
